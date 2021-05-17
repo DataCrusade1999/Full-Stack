54d4e4b53704ab808c2cdec23a2bbd1e6d90b881
DataCrusade1999 <ashutosh.pandeyhlr007@gmail.com>
2021-05-17 13:00:12 +0530

    added functions

---
+++ JS/functions.js
@@ -0,0 +1,13 @@
+/* 
+Immediately Invokable Function Expression
+You can create a function and immediately call it as shown below:
+This can be extremely useful for scoping variables and containing code.
+*/
+
+
+
+(function() {
+
+    console.log("Hello world");
+
+})();
