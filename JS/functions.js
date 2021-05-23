acd3c59aa05f473308a0a01ec9b59fecf15b8077
DataCrusade1999 <ashutosh.pandeyhlr007@gmail.com>
2021-05-23 16:46:56 +0530

    added function

--- JS/functions.js
+++ JS/functions.js
@@ -39,3 +39,29 @@ const sumOfNaturalNumber = (start,end) => {
 }
 
 console.log(sumOfNaturalNumber(1,100));
+
+/*
+Function with default parameter.
+*/
+function greetings(name = 'XYZ') {
+    let message = `${name}, welcome to 30 Days Of JavaScript!`
+    return message
+  }
+  
+  console.log(greetings())
+  console.log(greetings('Ashutosh Pandey'))
+
+/* 
+This function takes array as a parameter and sum up the numbers in the array(function with multiple arguments)
+
+*/
+function sumArrayValues(arr) {
+    let sum = 0;
+    for (let i = 0; i < arr.length; i++) {
+      sum = sum + arr[i];
+    }
+    return sum;
+  }
+  const numbers = [1, 2, 3, 4, 5];
+      //calling a function
+  console.log(sumArrayValues(numbers));
