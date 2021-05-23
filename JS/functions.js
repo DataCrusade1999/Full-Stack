6c64ddce21a64917dfcdad40b740f6908e9cff33
DataCrusade1999 <ashutosh.pandeyhlr007@gmail.com>
2021-05-23 16:36:23 +0530

    added flexbox

--- JS/functions.js
+++ JS/functions.js
@@ -11,3 +11,27 @@ This can be extremely useful for scoping variables and containing code.
     console.log("Hello world");
 
 })();
+
+
+/*
+Arrow function does not have the function scoped arguments object.
+To implement a function which takes unlimited number of arguments in an arrow function we use spread operator followed by any parameter name.
+Any thing we passed as argument in the function can be accessed as array in the arrow function. Let us see an example
+*/
+
+const areaOfCircle = (...radius) => {
+    console.log(radius);
+}
+
+areaOfCircle(23,4,5,6,7,8,9);
+
+const sumOfNaturalNumber = (start,end) => {
+    let sum = 0;
+    for (let index = start; index <= end; index++) {
+        sum = sum + index;
+    }
+
+    return sum;
+}
+
+console.log(sumOfNaturalNumber(1,100));
